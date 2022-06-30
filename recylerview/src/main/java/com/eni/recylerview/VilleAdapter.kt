76413912ba819c9fa22c516dce8d7ea667c3ac50
quentinrouet@hotmail.fr <quentinrouet@hotmail.fr>
2022-06-30 09:29:13 +0200

    recyclerview avec data binding

--- recylerview/src/main/java/com/eni/recylerview/VilleAdapter.kt
+++ recylerview/src/main/java/com/eni/recylerview/VilleAdapter.kt
@@ -1,31 +1,26 @@
 package com.eni.recylerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.eni.recylerview.databinding.ItemListVilleBinding
 
 class VilleAdapter(val listville : ArrayList<Ville>) :
     RecyclerView.Adapter<VilleAdapter.VilleViewHolder>() {
 
     //Va être de le template d'un élément de liste (va être réutiliser pour afficher
     // chaque ville
-    class VilleViewHolder(val itemView: View)  : RecyclerView.ViewHolder(itemView){
-        val textViewNomVille : TextView = itemView.findViewById(R.id.textViewNomVille);
-        val textViewPopulation: TextView = itemView.findViewById(R.id.textViewPopulation);
-    }
+    class VilleViewHolder(val binding: ItemListVilleBinding)  : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VilleViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_list_ville,parent,false)
-        return VilleViewHolder(itemView)
+        //val itemView = LayoutInflater.from(parent.context)
+        //    .inflate(R.layout.item_list_ville,parent,false)
+        var binding = ItemListVilleBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return VilleViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: VilleViewHolder, position: Int) {
-        var villeAAfficher = listville.get(position)
-        holder.textViewNomVille.text = villeAAfficher.nom
-        holder.textViewPopulation.text = villeAAfficher.population.toString()
+        holder.binding.ville = listville.get(position)
         //holder.itemView.setOnClickListener {
         //    Toast.makeText(holder.itemView.context, villeAAfficher.nom, Toast.LENGTH_SHORT).show()
         //}
